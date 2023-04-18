e7e85489ef90b596c70010271029e227b84addf2
Aramassa <52207597+Aramassa@users.noreply.github.com>
2023-04-18 13:57:02 +0000

    function として参照することに成功

--- 0_3/src/components/MyButton.jsx
+++ 0_3/src/components/MyButton.jsx
@@ -4,9 +4,10 @@ import Main from '../modules/app/main'
 const m = Main.singleton();
 
 export const Button = ()=>{
+
   return (
-    <button onClick={()=>{alert(m.test()) }}>
-      hello
+    <button onClick={()=>{m.test()}}>
+      hello {m.useCntState()}
     </button>
   )
 } 
--- 0_3/src/modules/app/main.ts
+++ 0_3/src/modules/app/main.ts
@@ -1,8 +1,26 @@
+import { useState, useEffect } from "react";
+
+const useCntState = () => {
+  const [count, setCount] = useState(0);
+
+  // 1秒ずつカウントを増やす
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 100);
+
+    // コンポーネントがアンマウントされる時にタイマー停止(クリーンアップ処理)
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return count;
+}
+
 export default class Main{
 
   private static instance?:Main;
-  
-  private num:number = 0;
 
   public static singleton(){
     if(!Main.instance) Main.instance = new Main();
@@ -10,6 +28,10 @@ export default class Main{
   }
 
   public test(){
-    return this.num++;
+    alert('test')
+  }
+
+  public get useCntState(){
+    return useCntState;
   }
 }
