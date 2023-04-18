d433751d1b8b50e392bc407da18356aaf1fca256
Aramassa <52207597+Aramassa@users.noreply.github.com>
2023-04-18 13:39:38 +0000

    add client side event

--- 0_3/src/components/MyButton.jsx
+++ 0_3/src/components/MyButton.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Main from '../modules/app/main'
 
 export const Button = ()=>{
   return (
-    <button>
+    <button onClick={()=>{alert(new Main().test(8)) }}>
       hello
     </button>
   )
